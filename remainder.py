e491175d6df18d29e4b54385c0cd9690ec7bc832
omkar_dere <omkardere021@gmail.com>
2024-07-09 20:59:20 +0530

    new files

---
+++ remainder.py
@@ -0,0 +1,10 @@
+#  The code for find out remainder 
+a=99
+b=10
+print("Remainder when a is divided by b  is",a % b )
+
+#   for user input 
+
+e =int(input("Enter the number 1 :"))
+f =int(input("Enter the number 2 :"))
+print("Remainder when a is divided by b  is",e % f )
