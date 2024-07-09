b2868a11b76e9778b914a790862f662f25c753fe
omkar_dere <omkardere021@gmail.com>
2024-07-09 21:04:52 +0530

    new files

--- remainder.py
+++ remainder.py
@@ -8,3 +8,17 @@ print("Remainder when a is divided by b  is",a % b )
 e =int(input("Enter the number 1 :"))
 f =int(input("Enter the number 2 :"))
 print("Remainder when a is divided by b  is",e % f )
+
+
+# find average of two number 
+
+g =int(input("Enter the number 1 :"))
+h =int(input("Enter the number 2 :"))
+print("Average of two number is", (g + h) / 2)
+
+
+# squre of two numbers 
+
+s =int(input("Enter the number 1 :"))
+print("Squre of two numbers",s*s)
+
