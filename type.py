b419098c32a0aa3041ef6052e52a400d52b6b2e0
omkar_dere <omkardere021@gmail.com>
2024-07-09 20:39:27 +0530

    new files

---
+++ type.py
@@ -0,0 +1,21 @@
+#  find the type of number 
+
+a= 223
+t=type(a)# <class 'int'>
+print(t)
+
+b=23.76
+s=type(b)# <class 'float'>
+print(s)
+
+c='hello'
+d=type(c)# <class 'str'>
+print(d)
+
+e='34.56'
+g=type(e) #<class 'str'>
+print(g)
+f=float(e) #<class change str to float >
+h=type(f)
+print(h)
+print(f)
