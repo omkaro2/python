00b25fe2a951372f332e66a5b9a161f1e1ffae26
omkar_dere <omkardere021@gmail.com>
2024-07-11 22:24:52 +0530

    new files

---
+++ string.py
@@ -0,0 +1,11 @@
+# string is data type in python 
+name= "Ramraj"
+nameshort = name[0:3]# start from index 0 all the way till 3
+print(nameshort) 
+print(name[-4:-1])
+
+# clicing with skip value 
+# run on windo terminal 
+# word="amazing"
+# word[1:7:5]
+# print(w)
